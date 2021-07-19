4fd87e5059aee31c8049ad2c9980c9cef02a703f
Stefan Gabriels <stefan.gabriels@gmail.com>
2021-07-20 00:17:32 +0200

    Fixed setup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import BootstrapVue from 'bootstrap-vue';
 
 Vue.use(BootstrapVue);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
